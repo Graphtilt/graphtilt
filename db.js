ed197cd33e23eb16d1cebb14fb9559acf5b063c2
prealge <34986064+prealge@users.noreply.github.com>
2018-10-24 19:05:32 -0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCc2gi73jeuiEBAffkLHtcUj8breTJI0ew",
+  authDomain: "graphtilt-490e5.firebaseapp.com",
+  databaseURL: "https://graphtilt-490e5.firebaseio.com",
+  projectId: "graphtilt-490e5",
+  storageBucket: "",
+  messagingSenderId: "265134588195"
+};
+firebase.initializeApp(config);
